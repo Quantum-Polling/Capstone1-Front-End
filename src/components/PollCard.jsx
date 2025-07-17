cc22d92eabac1cbb518101b187dac03ceebcda9e
pedrosortega <psortega2004@gmail.com>
2025-07-17 17:44:17 -0400

    added 1/2 of logic to display # of poll options

--- src/components/PollCard.jsx
+++ src/components/PollCard.jsx
@@ -6,12 +6,16 @@ const PollCard = ({ poll }) => {
     <div className="poll-card">
       <div className="poll-title">{poll.title}</div>
       <div className="poll-author">
-        Created by: {poll.creator.creatorFirstName}{" "}
-        {poll.creator.creatorLastName}
+        {/* Created by: {poll.creatorId.creator.creatorFirstName}{" "}
+        {poll.creatorId.creatorLastName} */}
       </div>
       <div className="poll-close-date">
         Closes: {poll.close_date ? poll.close_date : Manually}
       </div>
+      <div className="poll-auth-votes">
+        {poll.authVotes ? "Open to all" : "Logged in Users Only"}
+      </div>
+      <div className="poll-options">{poll.poll_options.length} options</div>
     </div>
   );
 };
