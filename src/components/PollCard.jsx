3d9fbdd304978a6e5332831525999f4e0a20bfcc
pedrosortega <psortega2004@gmail.com>
2025-07-18 14:39:21 -0400

    front end half of viewing pollcards. other half of logic is on the backend

--- src/components/PollCard.jsx
+++ src/components/PollCard.jsx
@@ -6,16 +6,21 @@ const PollCard = ({ poll }) => {
     <div className="poll-card">
       <div className="poll-title">{poll.title}</div>
       <div className="poll-author">
-        {/* Created by: {poll.creatorId.creator.creatorFirstName}{" "}
-        {poll.creatorId.creatorLastName} */}
+        Created by: {poll.creator.creatorFirstName}{" "}
+        {poll.creator.creatorLastName}
       </div>
       <div className="poll-close-date">
-        Closes: {poll.close_date ? poll.close_date : Manually}
+        Closes: {poll.close_date ? poll.close_date : "Manually"}
       </div>
       <div className="poll-auth-votes">
         {poll.authVotes ? "Open to all" : "Logged in Users Only"}
       </div>
-      <div className="poll-options">{poll.poll_options.length} options</div>
+      <div className="poll-options">
+        {poll.poll_options.length} options
+        <div className="total-poll-votes">
+          {poll.poll_votes.length} total votes
+        </div>
+      </div>
     </div>
   );
 };
