cee6746b9b4a705d535b787f871c8451246c0d09
Joseph Collado <sourpina@gmail.com>
2025-07-24 16:17:20 -0400

    Added first round data visualization

--- src/components/PollResults.jsx
+++ src/components/PollResults.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { API_URL } from "../shared";
+import { PieChart } from "@mui/x-charts/PieChart";
 import axios from "axios";
+import "./PollStyles.css";
+import Stack from "@mui/material/Stack";
+
 
 const PollResults = () => {
   const { pollId } = useParams();
 
   const [loading, setLoading] = useState(true);
   const [results, setResults] = useState([]);
+  const [round, setRound] = useState(-1);
+
+  /* REMOVE LATER */
+  const [options, setOptions] = useState([]);
 
   const loadResults = async () => {
     if (!pollId)
@@ -20,10 +28,11 @@ const PollResults = () => {
       const pollResponse = await axios.get(`${API_URL}/api/polls/${pollId}`);
       poll = pollResponse.data.poll;
       options = poll.poll_options;
+      setOptions(options);
       console.log("POLL RESULTS OPTIONS:", options);
     } catch (error) {
       console.error(error);
-      return;
+      return <p>{error}</p>;
     }
     /* END REMOVE LATER */
 
@@ -31,6 +40,7 @@ const PollResults = () => {
     const results = response.data.results;
     console.log("POLL RESULTS:", results);
     setResults(results);
+    setRound(0);
     setLoading(false);
   };
 
@@ -40,6 +50,43 @@ const PollResults = () => {
 
   if (loading)
     return <p>Loading...</p>
+
+  const getData = () => {
+    return results[round].map((roundResult, index) => (
+      {
+        id: index,
+        value: roundResult,
+        label: options[index].text,
+      }
+    ));
+  }
+
+  return (
+    <div className="poll-results drop-shadow">
+      <div className="previous drop-shadow">
+        <div className="arrow left"></div>
+      </div>
+      <div className="next drop-shadow">
+        <div className="arrow right"></div>
+      </div>
+      <h5>Hover to view individual results</h5>
+      <PieChart 
+        series={[
+          {
+            innerRadius: 45,
+            outerRadius: 60,
+            startAngle: -100,
+            endAngle: 100,
+            data: [...getData()],
+          },
+        ]}
+        width={250}
+        height={150}
+        hideLegend
+      />
+      <h1>Round {round + 1}</h1>
+    </div>
+  );
 };
 
 export default PollResults;
