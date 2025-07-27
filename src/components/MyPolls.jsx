edfab342ac90d6e4bccc6079c0681cf7b7445e47
Joseph <sourpina@gmail.com>
2025-07-27 03:38:26 -0400

    Changed Link to div with onClick that uses navigate

--- src/components/MyPolls.jsx
+++ src/components/MyPolls.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./MyPolls.css";
 
 const MyPolls = ({ polls, getPolls }) => {
-  const deleteDraft = () => {
+  const navigate = useNavigate();
 
+  const deleteDraft = (event) => {
+    const pollId = event.target.value;
     console.log("Delete Draft");
+    event.stopPropagation();
   }
 
-  const closePoll = () => {
+  const closePoll = (event) => {
+    const pollId = event.target.value;
     console.log("Close Poll");
+    event.stopPropagation();
   }
 
   return (
@@ -17,13 +22,13 @@ const MyPolls = ({ polls, getPolls }) => {
       <h2>All Polls</h2>
       {polls.length > 0 ? (
         polls.map((poll) => (
-          <Link 
-            to={`/polls/${poll.id}${
+          <div
+            onClick= {() => {navigate(`/polls/${poll.id}${
               poll.status === "Draft" ? "/edit" :
               poll.status === "Closed" ? "/results" : ""
-              }`
-            } 
-            key={poll.id} 
+              }`)
+            }}
+            key={poll.id}
             className="polls-link"
           >
             <div 
@@ -36,7 +41,7 @@ const MyPolls = ({ polls, getPolls }) => {
               <h3>{poll.title}</h3>
               <h4>{poll.description}</h4>
               <h5>{poll.status}</h5>
-              <div className="buttons">
+              <div className="actions">
                 { /* Delete Draft Button */
                   poll.status === "Draft" &&
                   <button
@@ -58,7 +63,7 @@ const MyPolls = ({ polls, getPolls }) => {
                 }
               </div>
             </div>
-          </Link>
+          </div>
         ))
       ) : (
         <p>No polls found</p>
