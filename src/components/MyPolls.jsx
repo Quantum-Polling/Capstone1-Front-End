540ba6f10163558f3c3658a630e6a6dd4bceadf5
Joseph <sourpina@gmail.com>
2025-07-27 03:05:48 -0400

    Added buttons to delete draft and manually close polls

--- src/components/MyPolls.jsx
+++ src/components/MyPolls.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./MyPolls.css";
 
-const MyPolls = ({ polls }) => {
+const MyPolls = ({ polls, getPolls }) => {
+  const deleteDraft = () => {
+
+    console.log("Delete Draft");
+  }
+
+  const closePoll = () => {
+    console.log("Close Poll");
+  }
+
   return (
     <div className="polls-list">
       <h2>All Polls</h2>
       {polls.length > 0 ? (
         polls.map((poll) => (
-          <Link to={`/polls/${poll.id}`} key={poll.id} className="polls-link">
+          <Link 
+            to={`/polls/${poll.id}`} 
+            key={poll.id} 
+            className="polls-link"
+          >
             <div 
               className={`polls-card
                 ${poll.disabled ? "disabled" : 
@@ -19,6 +32,27 @@ const MyPolls = ({ polls }) => {
               <h3>{poll.title}</h3>
               <h4>{poll.description}</h4>
               <h5>{poll.status}</h5>
+              <div className="buttons">
+                { /* Delete Draft Button */
+                  poll.status === "Draft" &&
+                  <button
+                    value={poll.id}
+                    onClick={deleteDraft}
+                  >
+                    Delete Draft
+                  </button>
+                }
+                { /* Manually Close Poll Button */
+                  poll.status === "Open" &&
+                  poll.close_date === null &&
+                  <button 
+                    value={poll.id}
+                    onClick={closePoll}
+                  >
+                    Close Poll
+                  </button>
+                }
+              </div>
             </div>
           </Link>
         ))
